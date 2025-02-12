f518ada3a34bdf17ddc41a859b31d9ef6fc75593
Vincent Fretin <vincent.fretin@gmail.com>
2025-02-12 11:24:25 +0100

    fix typescript errors so that it builds

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,15 +1,31 @@
 import { onCleanup, onMount } from "solid-js";
 import { primitive } from "./signals";
 
+declare module "solid-js" {
+  namespace JSX {
+    interface IntrinsicElements {
+      "a-scene": any;
+      "a-entity": any;
+      "a-assets": any;
+      "a-asset-item": any;
+      "a-mixin": any;
+      "a-sphere": any;
+      "a-torus": any;
+      "a-gltf-model": any;
+      "a-light": any;
+    }
+  }
+}
+
 export function Scene() {
   onMount(() => {
     // Because a-scene is in a div, we need this additional style otherwise the VR button is not visible.
-    document.querySelector("#root").setAttribute("style", "position: absolute; inset: 0;");
+    document.querySelector("#root")!.setAttribute("style", "position: absolute; inset: 0;");
   });
   onCleanup(() => {
-    document.querySelector("#root").removeAttribute("style");
+    document.querySelector("#root")!.removeAttribute("style");
     // Remove class that was added by aframe, otherwise we can't scroll when we go back to a non- A-Frame page. (not needed in aframe 1.7.0)
-    document.querySelector("html").classList.remove("a-fullscreen");
+    document.querySelector("html")!.classList.remove("a-fullscreen");
   });
   return (
     <a-scene
