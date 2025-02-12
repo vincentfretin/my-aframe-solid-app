b6a4f5b6e84577bc270b56db676c5b2765875625
Vincent Fretin <vincent.fretin@gmail.com>
2025-02-12 11:03:14 +0100

    move all buttons in a UI component so hot reload works, VR button now showing

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,38 +1,41 @@
-import { onCleanup } from "solid-js";
-import { Portal } from "solid-js/web";
+import { onCleanup, onMount } from "solid-js";
+import { primitive } from "./signals";
 
-export function Scene(props: { model: string }) {
+export function Scene() {
+  onMount(() => {
+    // Because a-scene is in a div, we need this additional style otherwise the VR button is not visible.
+    document.querySelector("#root").setAttribute("style", "position: absolute; inset: 0;");
+  });
   onCleanup(() => {
+    document.querySelector("#root").removeAttribute("style");
     // Remove class that was added by aframe, otherwise we can't scroll when we go back to a non- A-Frame page. (not needed in aframe 1.7.0)
     document.querySelector("html").classList.remove("a-fullscreen");
   });
   return (
-    <Portal>
-      <a-scene
-        renderer="physicallyCorrectLights: true;"
-        networked-scene="
+    <a-scene
+      renderer="physicallyCorrectLights: true;"
+      networked-scene="
           room: basic;
           adapter: wseasyrtc;"
-      >
-        <a-entity id="cameraRig">
-          <a-entity
-            id="player"
-            networked="template:#avatar-template;attachTemplateToLocal:false;"
-            camera
-            position="0 1.6 0"
-            spawn-in-circle="radius:3"
-            wasd-controls
-            look-controls
-          >
-            <a-sphere class="head" visible="false" random-color></a-sphere>
-          </a-entity>
+    >
+      <a-entity id="cameraRig">
+        <a-entity
+          id="player"
+          networked="template: #avatar-template; attachTemplateToLocal: false;"
+          camera
+          position="0 1.6 0"
+          spawn-in-circle="radius:3"
+          wasd-controls
+          look-controls
+        >
+          <a-sphere class="head" visible="false" random-color></a-sphere>
         </a-entity>
+      </a-entity>
 
-        <a-entity environment="preset:arches"></a-entity>
-        <a-entity light="type:ambient;intensity:1.0"></a-entity>
+      <a-entity environment="preset: arches"></a-entity>
+      <a-entity light="type: ambient; intensity: 1.0"></a-entity>
 
-        <a-entity attr:gltf-model={props.model} shadow="cast: true; receive: true"></a-entity>
-      </a-scene>
-    </Portal>
+      <a-entity attr:geometry={`primitive: ${primitive()}`} position="0 2 0"></a-entity>
+    </a-scene>
   );
 }
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,34 +1,11 @@
-import { createSignal } from "solid-js";
 import { Scene } from "../Scene";
+import { UI } from "../UI";
 
 function Room() {
-  const [model, setModel] = createSignal("my-model.glb");
   return (
     <>
-      <Scene model={model()} />
-      <div class="pointer-events-none absolute bottom-4 left-4 z-10 flex gap-2 [&>*]:pointer-events-auto">
-        <button
-          onclick={() => {
-            const el = document.querySelector("[environment]");
-            if (!el) return;
-            // @ts-ignore
-            if (el.getAttribute("environment").preset === "forest") {
-              el.setAttribute("environment", "preset: arches");
-            } else {
-              el.setAttribute("environment", "preset: forest");
-            }
-          }}
-          class="cursor-pointer rounded-xl border-4 border-black/80 bg-white px-4 py-1 font-bold text-black/80 hover:border-[#ef2d5e] hover:text-[#ef2d5e]"
-        >
-          Change environment
-        </button>
-        <button
-          onclick={() => setModel("my-other-model.glb")}
-          class="cursor-pointer rounded-xl border-4 border-black/80 bg-white px-4 py-1 font-bold text-black/80 hover:border-[#ef2d5e] hover:text-[#ef2d5e]"
-        >
-          Change model
-        </button>
-      </div>
+      <Scene />
+      <UI />
     </>
   );
 }
